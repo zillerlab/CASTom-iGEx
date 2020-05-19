c77f858d5467197746cadba048feba95091bada6
luciat <lucia_trastulla@psych.mpg.de>
2020-05-19 10:25:17 +0200

    correct script

--- Software/model_prediction/example/PriLer_phenoAssociation_CMC_prepare_phenoSpec.sh
+++ Software/model_prediction/example/PriLer_phenoAssociation_CMC_prepare_phenoSpec.sh
@@ -15,4 +15,4 @@ fold_train=OUTPUT_CMC/train_CMC/200kb/
 fold=OUTPUT_CMC/predict_UKBB/200kb/devgeno0.01_testdevgeno0/${name_pheno}_pheno/
 ref_fold=/psycl/g/mpsziller/lucia/refData/
 
-Rscript RSCRIPTS/SCRIPTS_v2/pheno_association_prepare_largeData_run.R --split_tot 100 --geneAnn_file ${fold_train}resPrior_regEval_allchr.txt --inputFold ${fold} --outFold ${name_pheno} --GOterms_file ${ref_fold}GOterm_geneAnnotation_allOntologies.RData --reactome_file ${ref_fold}ReactomePathways.gmt  --sampleAnn_file INPUT_DATA/Covariates/covariateMatrix_${name_pheno}.txt 
+Rscript RSCRIPTS/SCRIPTS_v2/pheno_association_prepare_largeData_run.R --split_tot 100 --geneAnn_file ${fold_train}resPrior_regEval_allchr.txt --inputFold ${fold} --outFold ${fold} --GOterms_file ${ref_fold}GOterm_geneAnnotation_allOntologies.RData --reactome_file ${ref_fold}ReactomePathways.gmt  --sampleAnn_file INPUT_DATA/Covariates/covariateMatrix_${name_pheno}.txt 
