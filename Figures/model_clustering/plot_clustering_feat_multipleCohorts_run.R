feef5b536b710f7c95731fc5a9c52a1cb076d540
luciat <lucia_trastulla@psych.mpg.de>
2020-10-13 15:57:08 +0200

    add empty pathways

--- Figures/model_clustering/plot_clustering_feat_multipleCohorts_run.R
+++ Figures/model_clustering/plot_clustering_feat_multipleCohorts_run.R
@@ -58,24 +58,22 @@ outFold <- args$outFold
 #####################################################################################################################
 # functR <- '/home/luciat/priler_project/Software/model_clustering/clustering_functions.R'
 # type_cluster <- 'Cases'
-# pval_feat <- 0.05
-# pval_pheno <- 0.001
-# pval_treat <- 0.001
+# pval_feat <- 0.001
 # type_cluster_data <- 'tscore'
-# tissue_name <- 'Adrenal_Gland'
+# tissue_name <- 'Small_Intestine_Terminal_Ileum'
 # min_genes_path <- 5
 # pheno_name <- 'SCZ'
 # geneInfoFile <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/train_GTEx/%s/200kb/PGC_GWAS_bin1e-2/resPrior_regEval_allchr.txt', tissue_name)
 # 
-# clustFile <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/tscore_zscaled_clusterCases_PGmethod_HKmetric.RData', tissue_name)
+# clustFile <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/excludeMHC_tscore_zscaled_clusterCases_PGmethod_HKmetric.RData', tissue_name)
 # 
-# tscore_featRelFile <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/tscoreOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
+# tscore_featRelFile <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/excludeMHC_tscoreOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
 # 
-# pathR_featRelFile<- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/path_ReactomeOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
+# pathR_featRelFile<- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/excludeMHC_path_ReactomeOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
 # 
-# pathGO_featRelFile<- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/path_GOOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
+# pathGO_featRelFile<- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/excludeMHC_path_GOOriginal_tscoreClusterCases_featAssociation.RData', tissue_name)
 # 
-# outFold <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/', tissue_name)
+# outFold <- sprintf('/home/luciat/eQTL_PROJECT/OUTPUT_GTEx/predict_PGC/%s/200kb/PGC_GWAS_bin1e-2/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/excludeMHC_', tissue_name)
 # 
 # GOterms_file <- '/home/luciat/priler_project/refData/GOterm_geneAnnotation_allOntologies.RData'
 # reactome_file <- '/home/luciat/priler_project/refData/ReactomePathways.gmt'
@@ -159,80 +157,83 @@ tmp <- get(load(pathR_featRelFile))
 pathR_input <- tmp$scaleData
 identical(rownames(pathR_input), cl$id)
 
-keep_path <- unique(tmp$test_feat$feat[tmp$test_feat$pval_corr <= pval_feat])
-pathInfo <- tmp$res_pval
-pathInfo_keep <- pathInfo[match(keep_path, pathInfo[,1]),]
-pathInfo_keep <- pathInfo_keep[pathInfo_keep$ngenes_tscore >= min_genes_path,]
-pathInfo_keep$Zstat <- pathInfo_keep[,12]
-test_feat_pathR <- tmp$test_feat
-# filter out pathway based on shared percentage
-gs <- readGmt(reactome_file)
-gs <- gs[which(sapply(gs, function(x) x@reference %in% pathInfo_keep[,1]))]
-# annotate gs with the used genes
-for(i in 1:length(gs)){
-  gs[[i]]@data <- gs[[i]]@ids[gs[[i]]@ids %in% tscore_info$external_gene_name]
-}
-
-cor_mat <- cor(pathR_input[, match(pathInfo_keep[,1], colnames(pathR_input)), drop = F])
-# remove higly correlated (same gene or genes in LD) and pathways that share high percentage of genes
-if(any(abs(cor_mat[upper.tri(cor_mat)]) > 0.6)){
-  path_list <- apply(cor_mat, 1, function(x) abs(x)>0.6)
-  len_path <- c()
-  keep_path <- c()
-  for(j in 1:nrow(path_list)){
-    tmp_sel <- pathInfo_keep[pathInfo_keep[,1] %in% pathInfo_keep[path_list[j,],1],]
-    tmp_sel <-  tmp_sel[!tmp_sel[,1] %in% len_path, ]
-    len_path <- unique(c(len_path, tmp_sel[,1]))
-    keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
-  }
-  #print(keep_gene)
-  pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
+if(min(tmp$test_feat$pval_corr) <= pval_feat){
+  keep_path <- unique(tmp$test_feat$feat[tmp$test_feat$pval_corr <= pval_feat])
+  pathInfo <- tmp$res_pval
+  pathInfo_keep <- pathInfo[match(keep_path, pathInfo[,1]),]
+  pathInfo_keep <- pathInfo_keep[pathInfo_keep$ngenes_tscore >= min_genes_path,]
+  pathInfo_keep$Zstat <- pathInfo_keep[,12]
+  test_feat_pathR <- tmp$test_feat
+  # filter out pathway based on shared percentage
+  gs <- readGmt(reactome_file)
   gs <- gs[which(sapply(gs, function(x) x@reference %in% pathInfo_keep[,1]))]
-  perc_mat <- sapply(gs, function(y) sapply(gs, function(x) length(intersect(y@data, x@data))/length(union(y@data, x@data))))
-  if(any(perc_mat[upper.tri(perc_mat)] > 0.1)){
-    path_list <- apply(perc_mat, 1, function(x) x>0.1)
+  # annotate gs with the used genes
+  for(i in 1:length(gs)){
+    gs[[i]]@data <- gs[[i]]@ids[gs[[i]]@ids %in% tscore_info$external_gene_name]
+  }
+  
+  cor_mat <- cor(pathR_input[, match(pathInfo_keep[,1], colnames(pathR_input)), drop = F])
+  # remove higly correlated (same gene or genes in LD) and pathways that share high percentage of genes
+  if(any(abs(cor_mat[upper.tri(cor_mat)]) > 0.6)){
+    path_list <- apply(cor_mat, 1, function(x) abs(x)>0.6)
     len_path <- c()
     keep_path <- c()
     for(j in 1:nrow(path_list)){
-      tmp_sel <-  pathInfo_keep[pathInfo_keep[,1] %in% sapply(gs[path_list[j,]], function(x) x@reference),]
-      tmp_sel <- tmp_sel[!tmp_sel[,1] %in% len_path, ]
+      tmp_sel <- pathInfo_keep[pathInfo_keep[,1] %in% pathInfo_keep[path_list[j,],1],]
+      tmp_sel <-  tmp_sel[!tmp_sel[,1] %in% len_path, ]
       len_path <- unique(c(len_path, tmp_sel[,1]))
       keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
     }
-    #print(keep_path)
+    #print(keep_gene)
     pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
+    gs <- gs[which(sapply(gs, function(x) x@reference %in% pathInfo_keep[,1]))]
+    perc_mat <- sapply(gs, function(y) sapply(gs, function(x) length(intersect(y@data, x@data))/length(union(y@data, x@data))))
+    if(any(perc_mat[upper.tri(perc_mat)] > 0.1)){
+      path_list <- apply(perc_mat, 1, function(x) x>0.1)
+      len_path <- c()
+      keep_path <- c()
+      for(j in 1:nrow(path_list)){
+        tmp_sel <-  pathInfo_keep[pathInfo_keep[,1] %in% sapply(gs[path_list[j,]], function(x) x@reference),]
+        tmp_sel <- tmp_sel[!tmp_sel[,1] %in% len_path, ]
+        len_path <- unique(c(len_path, tmp_sel[,1]))
+        keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
+      }
+      #print(keep_path)
+      pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
+    }
   }
-}
-
-# any pathway have a better performance than the single genes reported?
-P <- length(unique(cl$gr))
-comp <- unique(test_feat_tscore$comp)
-impr_path <- c()
-for(i in 1:nrow(pathInfo_keep)){
-  tmp <- gs[[which(sapply(gs, function(x) x@reference == pathInfo_keep[i,1]))]]
-  test_feat_tscore_tmp <- test_feat_tscore[test_feat_tscore$feat %in% tmp@data,]
-  test_feat_path_tmp <- test_feat_pathR[test_feat_pathR$feat == pathInfo_keep[i,1], ]
-  test_feat_path_tmp <- test_feat_path_tmp[match(comp, test_feat_path_tmp$comp),]
-  tmp_gr_tscore <- lapply(comp, function(x) test_feat_tscore_tmp$pval[test_feat_tscore_tmp$comp == x])
-  tscore_path_comp <- mapply(function(x, y, z) all(x < y & z <= 0.05), y = tmp_gr_tscore, x = test_feat_path_tmp$pval, z = test_feat_path_tmp$pval_corr)
-  if(any(tscore_path_comp)){
-    print(i)
-    impr_path <- c(impr_path, pathInfo_keep[i,1])
+  
+  # any pathway have a better performance than the single genes reported?
+  P <- length(unique(cl$gr))
+  comp <- unique(test_feat_tscore$comp)
+  impr_path <- c()
+  for(i in 1:nrow(pathInfo_keep)){
+    tmp <- gs[[which(sapply(gs, function(x) x@reference == pathInfo_keep[i,1]))]]
+    test_feat_tscore_tmp <- test_feat_tscore[test_feat_tscore$feat %in% tmp@data,]
+    test_feat_path_tmp <- test_feat_pathR[test_feat_pathR$feat == pathInfo_keep[i,1], ]
+    test_feat_path_tmp <- test_feat_path_tmp[match(comp, test_feat_path_tmp$comp),]
+    tmp_gr_tscore <- lapply(comp, function(x) test_feat_tscore_tmp$pval[test_feat_tscore_tmp$comp == x])
+    tscore_path_comp <- mapply(function(x, y, z) all(x < y & z <= 0.05), y = tmp_gr_tscore, x = test_feat_path_tmp$pval, z = test_feat_path_tmp$pval_corr)
+    if(any(tscore_path_comp)){
+      print(i)
+      impr_path <- c(impr_path, pathInfo_keep[i,1])
+    }
   }
+  pathInfo_keep$impr <- F
+  pathInfo_keep$impr[pathInfo_keep[,1] %in% impr_path] <- T
+  
+  ### plot ###
+  pheat_pl_tot(pheno_name = pheno_name, mat_tscore = tscore_input, mat_path = pathR_input, pval_thr_est = 0.05,
+               info_feat_tscore = geneInfo_keep, info_feat_path = pathInfo_keep, cl = cl, 
+               test_feat_tscore = test_feat_tscore, test_feat_path = test_feat_pathR, 
+               cap = 3, res_pl = 250, width_pl = 14 + round(length(unique(cl$gr))*0.25), 
+               height_pl = round(7 + (nrow(geneInfo_keep)+nrow(pathInfo_keep))*0.1), 
+               outFile = sprintf('%stscoreOriginal_path_ReactomeOriginal_%sCluster%s',outFold,type_cluster_data,type_cluster))
+  
+  print('tscore-path_Reactome heatmap finished')
+}else{
+  print('no significant tscore-path_Reactome')
 }
-pathInfo_keep$impr <- F
-pathInfo_keep$impr[pathInfo_keep[,1] %in% impr_path] <- T
-
-### plot ###
-pheat_pl_tot(pheno_name = pheno_name, mat_tscore = tscore_input, mat_path = pathR_input, pval_thr_est = 0.05,
-             info_feat_tscore = geneInfo_keep, info_feat_path = pathInfo_keep, cl = cl, 
-             test_feat_tscore = test_feat_tscore, test_feat_path = test_feat_pathR, 
-             cap = 3, res_pl = 250, width_pl = 14 + round(length(unique(cl$gr))*0.25), 
-             height_pl = round(7 + (nrow(geneInfo_keep)+nrow(pathInfo_keep))*0.1), 
-             outFile = sprintf('%stscoreOriginal_path_ReactomeOriginal_%sCluster%s',outFold,type_cluster_data,type_cluster))
-
-print('tscore-path_Reactome heatmap finished')
-
 ############################################
 ######## block T-score + path_GO ###########
 ############################################
@@ -247,85 +248,87 @@ print(identical(tmp$res_pval$path_id, tmp$test_feat$feat))
 colnames(pathGO_input) <- tmp$res_pval$path
 test_feat_pathGO$feat <- tmp$res_pval$path
 
-keep_path <- unique(tmp$test_feat$feat[tmp$test_feat$pval_corr <= pval_feat])
-pathInfo <- tmp$res_pval
-pathInfo_keep <- pathInfo[match(keep_path, pathInfo[,1]),]
-pathInfo_keep <- pathInfo_keep[pathInfo_keep$ngenes_tscore >= min_genes_path + 5,]
-pathInfo_keep$Zstat <- pathInfo_keep[,14]
-pathInfo_keep[,1] <- pathInfo_keep$path
-
-# filter out pathway based on shared percentage
-go <- get(load(GOterms_file))
-go <- go[which(sapply(go, function(x) x$Term %in% pathInfo_keep[,1]))]
-# annotate go with the used genes
-for(i in 1:length(go)){
-  go[[i]]$new <- go[[i]]$geneIds[go[[i]]$geneIds %in% tscore_info$external_gene_name]
-}
-
-cor_mat <- cor(pathGO_input[, match(pathInfo_keep[,1], colnames(pathGO_input)), drop = F])
-# remove higly correlated (same gene or genes in LD) and pathways that share high percentage of genes
-if(any(abs(cor_mat[upper.tri(cor_mat)]) > 0.6)){
-  path_list <- apply(cor_mat, 1, function(x) abs(x)>0.6)
-  len_path <- c()
-  keep_path <- c()
-  for(j in 1:nrow(path_list)){
-    tmp_sel <- pathInfo_keep[pathInfo_keep[,1] %in% pathInfo_keep[path_list[j,],1],]
-    tmp_sel <-  tmp_sel[!tmp_sel[,1] %in% len_path, ]
-    len_path <- unique(c(len_path, tmp_sel[,1]))
-    keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
-  }
-  #print(keep_gene)
-  pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
-  go <- go[which(sapply(go, function(x) x$Term %in% pathInfo_keep[,1]))]
+if(min(tmp$test_feat$pval_corr) <= pval_feat){
   
-  perc_mat <- sapply(go, function(y) sapply(go, function(x) length(intersect(y$new, x$new))/length(union(y$new, x$new))))
-  if(any(perc_mat[upper.tri(perc_mat)] > 0.1)){
-    path_list <- apply(perc_mat, 1, function(x) x>0.1)
+  keep_path <- unique(tmp$test_feat$feat[tmp$test_feat$pval_corr <= pval_feat])
+  pathInfo <- tmp$res_pval
+  pathInfo_keep <- pathInfo[match(keep_path, pathInfo[,1]),]
+  pathInfo_keep <- pathInfo_keep[pathInfo_keep$ngenes_tscore >= min_genes_path + 5,]
+  pathInfo_keep$Zstat <- pathInfo_keep[,14]
+  pathInfo_keep[,1] <- pathInfo_keep$path
+  
+  # filter out pathway based on shared percentage
+  go <- get(load(GOterms_file))
+  go <- go[which(sapply(go, function(x) x$Term %in% pathInfo_keep[,1]))]
+  # annotate go with the used genes
+  for(i in 1:length(go)){
+    go[[i]]$new <- go[[i]]$geneIds[go[[i]]$geneIds %in% tscore_info$external_gene_name]
+  }
+  
+  cor_mat <- cor(pathGO_input[, match(pathInfo_keep[,1], colnames(pathGO_input)), drop = F])
+  # remove higly correlated (same gene or genes in LD) and pathways that share high percentage of genes
+  if(any(abs(cor_mat[upper.tri(cor_mat)]) > 0.6)){
+    path_list <- apply(cor_mat, 1, function(x) abs(x)>0.6)
     len_path <- c()
     keep_path <- c()
     for(j in 1:nrow(path_list)){
-      tmp_sel <-  pathInfo_keep[pathInfo_keep[,1] %in% sapply(go[path_list[j,]], function(x) x$Term),]
-      tmp_sel <- tmp_sel[!tmp_sel[,1] %in% len_path, ]
+      tmp_sel <- pathInfo_keep[pathInfo_keep[,1] %in% pathInfo_keep[path_list[j,],1],]
+      tmp_sel <-  tmp_sel[!tmp_sel[,1] %in% len_path, ]
       len_path <- unique(c(len_path, tmp_sel[,1]))
       keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
     }
-    #print(keep_path)
+    #print(keep_gene)
     pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
+    go <- go[which(sapply(go, function(x) x$Term %in% pathInfo_keep[,1]))]
+    
+    perc_mat <- sapply(go, function(y) sapply(go, function(x) length(intersect(y$new, x$new))/length(union(y$new, x$new))))
+    if(any(perc_mat[upper.tri(perc_mat)] > 0.1)){
+      path_list <- apply(perc_mat, 1, function(x) x>0.1)
+      len_path <- c()
+      keep_path <- c()
+      for(j in 1:nrow(path_list)){
+        tmp_sel <-  pathInfo_keep[pathInfo_keep[,1] %in% sapply(go[path_list[j,]], function(x) x$Term),]
+        tmp_sel <- tmp_sel[!tmp_sel[,1] %in% len_path, ]
+        len_path <- unique(c(len_path, tmp_sel[,1]))
+        keep_path <- unique(c(keep_path, tmp_sel[which.max(abs(tmp_sel$Zstat)), 1]))
+      }
+      #print(keep_path)
+      pathInfo_keep <- pathInfo_keep[pathInfo_keep[,1] %in% keep_path, ]
+    }
   }
-}
-
-# any pathway have a better performance than the single genes reported?
-P <- length(unique(cl$gr))
-comp <- unique(test_feat_tscore$comp)
-impr_path <- c()
-for(i in 1:nrow(pathInfo_keep)){
-  tmp <- go[[which(sapply(go, function(x) x$Term == pathInfo_keep[i,1]))]]
-  test_feat_tscore_tmp <- test_feat_tscore[test_feat_tscore$feat %in% tmp$new,]
-  test_feat_path_tmp <- test_feat_pathGO[test_feat_pathGO$feat == pathInfo_keep[i,1], ]
-  test_feat_path_tmp <- test_feat_path_tmp[match(comp, test_feat_path_tmp$comp),]
-  tmp_gr_tscore <- lapply(comp, function(x) test_feat_tscore_tmp$pval[test_feat_tscore_tmp$comp == x])
-  tscore_path_comp <- mapply(function(x, y, z) all(x < y & z <= 0.05), y = tmp_gr_tscore, x = test_feat_path_tmp$pval, z = test_feat_path_tmp$pval_corr)
-  if(any(tscore_path_comp)){
-    print(i)
-    impr_path <- c(impr_path, pathInfo_keep[i,1])
+  
+  # any pathway have a better performance than the single genes reported?
+  P <- length(unique(cl$gr))
+  comp <- unique(test_feat_tscore$comp)
+  impr_path <- c()
+  for(i in 1:nrow(pathInfo_keep)){
+    tmp <- go[[which(sapply(go, function(x) x$Term == pathInfo_keep[i,1]))]]
+    test_feat_tscore_tmp <- test_feat_tscore[test_feat_tscore$feat %in% tmp$new,]
+    test_feat_path_tmp <- test_feat_pathGO[test_feat_pathGO$feat == pathInfo_keep[i,1], ]
+    test_feat_path_tmp <- test_feat_path_tmp[match(comp, test_feat_path_tmp$comp),]
+    tmp_gr_tscore <- lapply(comp, function(x) test_feat_tscore_tmp$pval[test_feat_tscore_tmp$comp == x])
+    tscore_path_comp <- mapply(function(x, y, z) all(x < y & z <= 0.05), y = tmp_gr_tscore, x = test_feat_path_tmp$pval, z = test_feat_path_tmp$pval_corr)
+    if(any(tscore_path_comp)){
+      print(i)
+      impr_path <- c(impr_path, pathInfo_keep[i,1])
+    }
   }
+  pathInfo_keep$impr <- F
+  pathInfo_keep$impr[pathInfo_keep[,1] %in% impr_path] <- T
+  
+  ### plot ###
+  pheat_pl_tot(pheno_name = pheno_name, mat_tscore = tscore_input, mat_path = pathGO_input, pval_thr_est = 0.05,
+               info_feat_tscore = geneInfo_keep, info_feat_path = pathInfo_keep, cl = cl, 
+               test_feat_tscore = test_feat_tscore, test_feat_path = test_feat_pathGO, 
+               cap = 3, res_pl = 250,  width_pl = round(13 + length(unique(cl$gr))*0.25), 
+               height_pl = round(7 + (nrow(geneInfo_keep)+nrow(pathInfo_keep))*0.1), 
+               outFile = sprintf('%stscoreOriginal_path_GOOriginal_%sCluster%s', outFold,type_cluster_data,type_cluster))
+  
+  print('tscore-path_GO heatmap finished')
+  
+}else{
+  print('no significant tscore-path_GO')
 }
-pathInfo_keep$impr <- F
-pathInfo_keep$impr[pathInfo_keep[,1] %in% impr_path] <- T
-
-### plot ###
-pheat_pl_tot(pheno_name = pheno_name, mat_tscore = tscore_input, mat_path = pathGO_input, pval_thr_est = 0.05,
-             info_feat_tscore = geneInfo_keep, info_feat_path = pathInfo_keep, cl = cl, 
-             test_feat_tscore = test_feat_tscore, test_feat_path = test_feat_pathGO, 
-             cap = 3, res_pl = 250,  width_pl = round(13 + length(unique(cl$gr))*0.25), 
-             height_pl = round(7 + (nrow(geneInfo_keep)+nrow(pathInfo_keep))*0.1), 
-             outFile = sprintf('%stscoreOriginal_path_GOOriginal_%sCluster%s', outFold,type_cluster_data,type_cluster))
-
-print('tscore-path_GO heatmap finished')
-
-
-
-
 
 
 
