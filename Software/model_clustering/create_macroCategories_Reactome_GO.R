36f71e0fe4a449a7ce7c57333ba907c122481691
luciat <lucia_trastulla@psych.mpg.de>
2021-04-20 15:43:06 +0200

    add path to macro nodes

--- Software/model_clustering/create_macroCategories_Reactome_GO.R
+++ Software/model_clustering/create_macroCategories_Reactome_GO.R
@@ -11,13 +11,21 @@ class_names <- list_path[list_path$V1 %in% class_names, ]
 
 gr_R = graph_from_data_frame(df_R, directed = TRUE, vertices = NULL)
 distMatrix <- shortest.paths(gr_R, v=class_names$V1, to=V(gr_R))
+distMatrix_original <- distMatrix
+# some paths can have multiple macro, put Inf to the ones that are higher
+for(j in 1:ncol(distMatrix)){
+  if(sum(distMatrix[, j] != Inf)>1){
+    distMatrix[distMatrix[, j]!=min(distMatrix[, j]), j] <- Inf
+  }
+}
 group_class <- apply(distMatrix, 1, function(x) names(which(x!=Inf)))
 Reactome_macro <- list()
 for(i in 1:length(group_class)){
   Reactome_macro[[i]] <- list(macro = class_names[i, 1:2], 
                               subclasses = list_path[list_path$V1 %in% group_class[[i]], 1:2])
-  Reactome_macro[[i]]$subclasses$shortestPath <- distMatrix[i,match(Reactome_macro[[i]]$subclasses$V1,colnames(distMatrix))]
+  Reactome_macro[[i]]$subclasses$shortestPath <- distMatrix_original[i,match(Reactome_macro[[i]]$subclasses$V1,colnames(distMatrix_original))]
 }
+
 
 save(Reactome_macro, file = 'ReactomePathways_macro_2021.RData')
 
