139d06cfcd55eaf564f75b80d8a4963131d48538
luciat <lucia_trastulla@psych.mpg.de>
2022-04-06 23:55:31 +0200

    add all tissues

--- Figures/model_clustering/plot_precision_risk_score_groupSpec_run.R
+++ Figures/model_clustering/plot_precision_risk_score_groupSpec_run.R
@@ -33,9 +33,11 @@ color_tissues_file <- args$color_tissues_file
 outFold <- args$outFold
 
 ###################################################################################################################
-# tissues <- c('Liver', 'Heart_Left_Ventricle', 'Colon_Sigmoid')
-# riskScore_comp_file <- paste0('OUTPUT_GTEx/predict_CAD/',tissues,'/200kb/CAD_GWAS_bin5e-2/UKBB/devgeno0.01_testdevgeno0/CAD_HARD_clustering/riskScores_clusterCases_CAD_HARD_group_relatedPheno_measureGoodnessPred.txt')
-# outFold <- '/psycl/g/mpsziller/lucia/CAD_UKBB/eQTL_PROJECT/OUTPUT_GTEx/predict_CAD/AllTissues/200kb/CAD_GWAS_bin5e-2/UKBB/'
+# tissues <- c('Adipose_Subcutaneous','Adipose_Visceral_Omentum', 'Adrenal_Gland',
+# 'Artery_Aorta', 'Artery_Coronary', 'Colon_Sigmoid' ,'Colon_Transverse', 'Heart_Atrial_Appendage',
+# 'Heart_Left_Ventricle', 'Liver', 'Whole_Blood')
+# riskScore_comp_file <- paste0('OUTPUT_GTEx/predict_CAD/',tissues,'/200kb/CAD_GWAS_bin5e-2/UKBB/devgeno0.01_testdevgeno0/CAD_HARD_clustering/update_corrPCs/cp_riskScores_clusterCases_CAD_HARD_group_relatedPheno_measureGoodnessPred.txt')
+# outFold <- '/psycl/g/mpsziller/lucia/CAD_UKBB/eQTL_PROJECT/OUTPUT_GTEx/predict_CAD/AllTissues/200kb/CAD_GWAS_bin5e-2/UKBB/CAD_HARD_clustering/update_corrPCs/'
 # color_tissues_file <- '/psycl/g/mpsziller/lucia/priler_project/Figures/color_tissues.txt'
 #################################################################################################################
 
@@ -59,7 +61,7 @@ df_repr <- list()
 
 for(j in 1:length(thr_measure)){
   # print(j)
-  df_repr[[j]] <- data.frame(tissue = tissues, n_pheno_thr = NA, 
+  df_repr[[j]] <- data.frame(tissue = c(tissues, 'All tissues'), n_pheno_thr = NA, 
                              n_same_sign = NA, precision = NA, recall = NA, 
                              thr_measure = thr_measure[j])
   
@@ -82,18 +84,34 @@ for(j in 1:length(thr_measure)){
     }
   }
   
+  tmp <- df_tot
+  df_repr[[j]]$n_pheno_thr[i+1] <- sum(tmp$measure >= thr_measure[j])
+  df_repr[[j]]$n_same_sign[i+1] <- sum(tmp$same_sign == 1)
+  
+  if(sum(tmp$measure >= thr_measure[j]) > 0){
+    df_repr[[j]]$precision[i+1] <- sum(tmp$measure >= thr_measure[j] & tmp$same_sign == 1)/sum(tmp$measure >= thr_measure[j])
+  }else{
+    df_repr[[j]]$precision[i+1] <- NA
+  }
+  
+  if(sum(tmp$same_sign == 1) > 0){
+    df_repr[[j]]$recall[i+1] <- sum(tmp$measure >= thr_measure[j] & tmp$same_sign == 1)/sum(tmp$same_sign == 1)
+  }else{
+    df_repr[[j]]$recall[i+1] <- NA
+  }
+  
 }
 
 df_repr <- do.call(rbind, df_repr)
-df_repr$tissue <- factor(df_repr$tissue, levels = tissues)
+df_repr$tissue <- factor(df_repr$tissue, levels = c(tissues, "All tissues"))
 
 # thr <- max(sapply(tissues, function(x) min(df_repr$thr_measure[df_repr$precision == 1 & df_repr$tissue == x], na.rm  = T)))
-thr <- sapply(tissues, function(x) min(df_repr$thr_measure[df_repr$precision ==1 & df_repr$tissue == x], 
-                                           na.rm  = T))
+thr <- sapply(c(tissues, "All tissues"), function(x) min(df_repr$thr_measure[df_repr$precision ==1 & df_repr$tissue == x], 
+                                       na.rm  = T))
 if(any(!is.finite(thr))){
-  tissue_rep <- tissues[!is.finite(thr)]
+  tissue_rep <- c(tissues, "All tissues")[!is.finite(thr)]
   new_thr <- sapply(tissue_rep, function(x) min(df_repr$thr_measure[df_repr$precision == 0 & df_repr$tissue == x], 
-                                         na.rm  = T))
+                                                na.rm  = T))
   thr <- c(new_thr, thr[is.finite(thr)])
 }
 
@@ -109,7 +127,7 @@ pl1 <- ggplot(data =  tmp,aes(x = thr_measure, y = precision, color = tissue, gr
   theme_bw()+theme(legend.position = 'right', plot.title = element_text(hjust = 0.5), 
                    legend.text = element_text(size = 7),  legend.title = element_text(size = 8), legend.key.size = unit(0.3, "cm"))+
   guides(color=guide_legend(title = ''))+
-  scale_color_manual(values = color_tissues$color)
+  scale_color_manual(values = c(color_tissues$color, 'grey20'))
 
 pl2 <- ggplot(data =  tmp,aes(x = thr_measure, y = n_pheno_thr, color = tissue, group = tissue))+
   geom_point(alpha = 0.7)+
@@ -118,7 +136,7 @@ pl2 <- ggplot(data =  tmp,aes(x = thr_measure, y = n_pheno_thr, color = tissue, 
   theme_bw()+theme(legend.position = 'right', plot.title = element_text(hjust = 0.5), 
                    legend.text = element_text(size = 7),  legend.title = element_text(size = 8), legend.key.size = unit(0.3, "cm"))+
   guides(color=guide_legend(title = ''))+
-  scale_color_manual(values = color_tissues$color)
+  scale_color_manual(values = c(color_tissues$color, 'grey20'))
 
 tot_pl <- ggarrange(plotlist = list(pl1, pl2), ncol=2, nrow=1, align = 'h', common.legend = T, legend = 'right')
 ggsave(filename = sprintf('%sriskScores_clusterCases_group_relatedPheno_npheno_and_precision.png', outFold), plot = tot_pl, width = 9, height = 4.5, dpi = 500)
@@ -126,8 +144,5 @@ ggsave(filename = sprintf('%sriskScores_clusterCases_group_relatedPheno_npheno_a
 
 write.table(file = sprintf('%sriskScores_clusterCases_group_relatedPheno_npheno_and_precision.txt', outFold), 
             x = df_repr, col.names = T, row.names = F, sep = '\t', quote = F)
-
-
-
 
 
