7bf1caf1235b368e58fd96793ac715fc76727e60
luciat <lucia_trastulla@psych.mpg.de>
2023-07-20 10:04:20 +0200

    rm not needed libraries

--- Software/model_clustering/plot_precision_risk_score_groupSpec_run.R
+++ Software/model_clustering/plot_precision_risk_score_groupSpec_run.R
@@ -4,7 +4,6 @@
 options(stringsAsFactors=F)
 options(max.print=1000)
 suppressPackageStartupMessages(library(argparse))
-suppressPackageStartupMessages(library(e1071))
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(doParallel))
 suppressPackageStartupMessages(library(qvalue))
