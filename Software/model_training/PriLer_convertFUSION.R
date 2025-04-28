4a83d194e554eb062bc32293000bad39117c57e9
dolgalevgv <dolgalevgv@gmail.com>
2025-04-28 11:27:32 +0200

    Do not quote strings

--- Software/model_training/PriLer_convertFUSION.R
+++ Software/model_training/PriLer_convertFUSION.R
@@ -147,4 +147,10 @@ for (chr in 1:22) {
 
 fusion_pos <- do.call(rbind, fusion_pos)
 
-write.table(fusion_pos, sprintf("%s/%s.pos", args$outFold, args$modelName), sep = "\t", row.names = FALSE)
+write.table(
+  fusion_pos,
+  sprintf("%s/%s.pos", args$outFold, args$modelName),
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE
+)
